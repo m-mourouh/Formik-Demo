7c9606427311551560414aefc1411075bdec25fb
m-mourouh <mouhamedmourouh1999@gmail.com>
2022-11-05 13:26:58 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+// Components
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SignUpForm.tsx
@@ -0,0 +1,11 @@
+import { Formik } from "formik"
+
+const SignUpForm = () => {
+  return (
+    <form>
+      
+    </form>
+  )
+}
+
+export default SignUpForm
