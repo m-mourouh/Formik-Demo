fb8394c0941b8a9aaf2325b3b5e1850b5d4b9b39
m-mourouh <mouhamedmourouh1999@gmail.com>
2022-11-06 21:54:14 +0100

    last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 
 // Components
+import SignUpForm from './components/SignUpForm'
+
 
 function App() {
  
 
   return (
     <div className="App">
-      
+      <SignUpForm/>
     </div>
   )
 }
--- src/components/SignUpForm.tsx
+++ src/components/SignUpForm.tsx
@@ -1,9 +1,47 @@
-import { Formik } from "formik"
+import { useFormik } from "formik"
+import * as Yup from "Yup";  // Great form validation library  
 
 const SignUpForm = () => {
+    
+    const formik = useFormik({
+        initialValues: { 
+            firstName: "",
+             lastName: "",
+              email: ""
+            },
+            validationSchema: Yup.object({
+                firstName: Yup.string().max(15,"Must be 15 characters or less").required("First name is required"),
+                lastName: Yup.string().max(20,"Must be 15 characters or less").required("Last name is required"),
+                email:  Yup.string().email("Invalid Email address").required('Email is required')
+            }),
+            onSubmit: (values): void => {
+                console.log(values);
+            }
+    });
+    //! console log formik values
+    // console.log(formik.values);
+    //! console log formik values
+    console.log(formik.touched);
   return (
-    <form>
-      
+    <form className="loginForm"  onSubmit={formik.handleSubmit}>
+        <h1>Login</h1>
+
+        <span>
+            <input type="text" placeholder="First Name" value={formik.values.firstName} onChange={formik.handleChange} name="firstName" onBlur={formik.handleBlur} />
+        </span>
+        { formik.touched.firstName && formik.errors.firstName &&   <p className="error"><small>{formik.errors.firstName}</small></p> }
+        <span>
+            <input type="text" placeholder="Last Name" value={formik.values.lastName}  onChange={formik.handleChange} name="lastName" onBlur={formik.handleBlur} />
+       
+        </span>
+        { formik.touched.lastName && formik.errors.lastName &&   <p className="error"><small>{formik.errors.lastName}</small></p> }
+        <span>
+            <input type="email" placeholder="Email" value={formik.values.email} onChange={formik.handleChange} name="email" onBlur={formik.handleBlur} />
+       
+        </span>
+        {  formik.touched.email && formik.errors.email &&   <p className="error"><small>{formik.errors.email}</small></p> }
+        <button type="submit" className="btn">Login</button>
+
     </form>
   )
 }
